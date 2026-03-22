7786e12556af8e9b34b6ab5f91122a711fbd4fff
Keny2004 <keny20041117@gmail.com>
2026-03-22 13:32:41 +0800

    del all

--- carousell_bot/test_parse.py
+++
@@ -1,9 +0,0 @@
-import scraper
-from bs4 import BeautifulSoup
-from curl_cffi import requests
-
-res = requests.get("https://tw.carousell.com/search/iPhone%2014%20Pro?sort_by=3", impersonate="chrome")
-soup = BeautifulSoup(res.text, "html.parser")
-cards = soup.find_all("div", attrs={"data-testid": lambda x: x and x.startswith("listing-card-")})
-if cards:
-    print(cards[0].prettify()[:1500])
--- carousell_bot/test_scrape.py
+++
@@ -1,6 +0,0 @@
-import scraper
-import yaml
-
-items = scraper.fetch_carousell_items("iPhone 14 Pro 128G", sort_by="3")
-for item in items[:15]:
-    print(f"Title: {item['title']}, Price: {item['price']}")
